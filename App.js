71619cf3133ab609e93ff0496aa75174d28180ea
Daniel Knight <dtk3@kent.ac.uk>
2021-04-30 10:48:19 +0100

    Async storage init code

--- App.js
+++ App.js
@@ -1,21 +1,137 @@
-import { StatusBar } from 'expo-status-bar';
-import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import AsyncStorage from '@react-native-async-storage/async-storage';
+import React, { useEffect, useState } from 'react';
+import {
+  StyleSheet,
+  Text,
+  View,
+  TouchableOpacity,
+  FlatList,
+  SafeAreaView,
+} from 'react-native';
+
+//const DATA = AsyncStorage.getItem('test')
+
+
+
+
 
 export default function App() {
+  // const items = await AsyncStorage.getItem('test');
+  // const output = JSON.parse(items).list.map(obj => <Text>{obj.id}</Text>)
+
+  const [list, setList] = useState([]);
+  const getData = async () => {
+    try {
+      const jsonValue = await AsyncStorage.getItem('test')
+      if (jsonValue != null) {
+        setList(JSON.parse(jsonValue).list)
+      } else {
+        setList([])
+      }
+      //alert(jsonValue != null ? JSON.parse(jsonValue).list : null)
+      //console.log(JSON.parse(jsonValue))
+      //return jsonValue != null ? JSON.parse(jsonValue) : null;
+    } catch (e) {
+      // error reading value
+    }
+  }
+  const saveData = async (id) => {
+    try {
+      const oldData = await AsyncStorage.getItem('test');//retrieve old data
+      const jsonOldData = JSON.parse(oldData);//parse the string to JSON object
+      let JSON_Val;
+      //alert(jsonOldData)
+      if (jsonOldData === null) {//first time
+        //alert('empty')
+        JSON_Val = JSON.stringify({
+          list: [
+            { id: id }
+          ]
+        })
+        AsyncStorage.setItem('test', JSON_Val)
+      } else {//subsequent times added with old data
+        console.log(jsonOldData.list);
+        JSON_Val = JSON.stringify({
+          list: [
+            ...jsonOldData.list,//merge old with new obj
+            { id: id }
+          ]
+        })
+        AsyncStorage.setItem('test', JSON_Val)
+      }
+      getData()
+    } catch (e) {
+      // saving error
+    }
+  }
+  useEffect(() => {
+    getData()
+  }, [])
+
+
+  const clearStorage = async () => {
+    try {
+      await AsyncStorage.removeItem('test')
+      getData()
+    } catch (e) {
+      console.log(e)
+    }
+  }
+
+
+  const renderItem = ({ item }) => {//do {item} so you dont need to do item.item...
+    console.log(item)
+    return (
+      <View style={styles.renderItem}>
+        <Text style={{ textAlign: 'center' }}>{item.id}</Text>
+      </View>
+    )
+  }
+
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
+    <SafeAreaView style={styles.container}>
+      <TouchableOpacity
+        style={styles.spacing}
+        onPress={() => saveData(`id -> ${Math.random()}`)}>
+        <Text>CLICK TO SAVE RANDOM</Text>
+      </TouchableOpacity>
+
+      <TouchableOpacity
+        style={styles.spacing}
+        onPress={clearStorage}>
+        <Text>CLEAR LIST</Text>
+      </TouchableOpacity>
+
+      <FlatList
+        data={list}
+        renderItem={renderItem}
+        keyExtractor={item => item.id}
+      />
+    </SafeAreaView>
   );
 }
+
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
+    width: '100%', 
+    //backgroundColor: '#fff',
     alignItems: 'center',
     justifyContent: 'center',
   },
+
+  renderItem: {
+    backgroundColor: '#ccc',
+    width: '100%',
+    height: 50,
+    padding: 10,
+    justifyContent: 'center',
+    alignItems: 'center',
+    marginVertical: 20
+  },
+
+  spacing: {
+    marginVertical: 25
+  }
 });
